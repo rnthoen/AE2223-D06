c760432cd0b595959e2e5549045a491b4da3e1af
rnthoen <93116591+rnthoen@users.noreply.github.com>
2022-03-04 11:57:46 +0100

    Create separate_sequences.py

---
+++ separate_sequences.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+def separate_sequences(dataframe):
+    length = len(dataframe['count'])
+
+    # Create lists
+    cycle_number_list = []
+    start_count_list = []
+    end_count_list = []
+
+    # Set first values
+    cycle_number_list.append(500)
+    start_count_list.append(dataframe['count'][0])
+
+    # Define thresholds
+    time_jump = 5 # [s]
+    load_jump = 0 # [kN]
+    displacement_jump = 0 # [mm]
+
+    i = 0
+    while (i < (length - 1)):
+        test_results = []
+
+        # Test for time jump
+        if (dataframe['time'][i+1] - dataframe['time'][i]) > time_jump:
+            test_results.append(True)
+        else:
+            test_results.append(False)
+
+        # Test for load jump
+        if (dataframe['load'][i+1] - dataframe['load'][i]) > load_jump:
+            test_results.append(True)
+        else:
+            test_results.append(False)
+
+        # Test for displacement jump
+        if (dataframe['displacement'][i+1] - dataframe['displacement'][i]) > displacement_jump:
+            test_results.append(True)
+        else:
+            test_results.append(False)
+
+        # Check test results
+        if sum(test_results) == 3:
+            # Update previous end count
+            end_count = dataframe['count'][i]
+            end_count_list.append(end_count)
+
+            # Add new cycle number
+            j = len(cycle_number_list) - 1
+            cycle_number = 500 + 500 * (j + 1) + 500 * ((j + 1) // 9)
+            cycle_number_list.append(cycle_number)
+
+            # Add new start count
+            start_count = end_count + 1
+            start_count_list.append(start_count)
+
+        if (sum(test_results) == 2) or (sum(test_results) == 1):
+            # Print error message
+            location = dataframe['count'][i]
+            print(f'Error occured at count = {location}: {test_results}')
+
+        i += 1
+
+    # Update end count of last entry
+    end_count =  dataframe['count'][i]
+    end_count_list.append(end_count)
+
+    # Create dataframe
+    sequences_dataframe = pd.DataFrame({'cycle_number': cycle_number_list, 'start_count': start_count_list, 'end_count': end_count_list})
+
+    # Save dataframe as csv
+    sequences_dataframe.to_csv('separated_sequences.csv')
+
+    return sequences_dataframe
