fdfc0e979a3e60e4ebcca568b3f203ed557450d8
Thomas van de Pavoordt <thomas@marcing.nl>
2022-05-08 14:23:47 +0200

    Stress_field.py changed to remove outliers and upload a csv file with x,y,u and v. Generalised for all specimens and added comments for clarity.

--- Stress_field.py
+++ Stress_field.py
@@ -17,8 +17,9 @@ files = [['Data/L103/L1-03.csv', 'Data/L103/L1-03_0_2_4052.csv', 'Data/L103/L1-0
          ['Data/L123/L1-23.csv', 'Data/L123/L1-23_0_2_4000.csv', 'Data/L123/L1-23_4002_2_8080.csv', 'Data/L123/L1-23_8082_2_12430.csv', 'Data/L123/L1-23_12432_2_15850.csv', 'Data/L123/L1-23_15852_2_20506.csv', 'Data/L123/L1-23_20508_2_24236.csv', 'Data/L123/L1-23_24238_2_28894.csv', 'Data/L123/L1-23_28896_2_31384.csv', 'Data/L123/L1-23_31386_2_35416.csv', 'Data/L123/L1-23_35418_2_40390.csv', 'Data/L123/L1-23_40392_2_42256.csv']]
 
 ### Input space ###
-## Select which specimen you want ##
-select_specimen = files[0]
+### Select which specimen you want ###
+select_specimen = files[4]
+
 if select_specimen == files[0]:
     specimen = "L103"
 elif select_specimen == files[1]:
@@ -29,43 +30,18 @@ elif select_specimen == files[3]:
     specimen = "L109"
 else: specimen = "L123"
 
-## Select which cycle numbers you want to see ##
-cycle_number_list = [150500]
+### Select which cycle number you want to see ###
+cycle_number_list = [420500]
 
 ### Imports MTS data and DIC data and sequences ###
 df_data = import_data(select_specimen)
-#df_data[1].to_csv(f'Data/Ben.csv')
 df_separated = separate_sequences(df_data[0])
 
 ### Define variables ###
-nu = 0.33
-E = 1
-
-#print(df_data[1])
+nu = 0.33 #Poisson ratio
+E = 1 #Young's Modulus
 
 ### Find start and end in df_separated for cycle_number ###
-# for cycle_number in cycle_number_list:
-#     idx = df_separated.cycle_number[df_separated.cycle_number == cycle_number].index.tolist()
-#     start_count = int(df_separated.start_count[idx])
-#     end_count = int(df_separated.end_count[idx])
-#     total_count = end_count - start_count
-#     real_count = int(total_count/10)
-#     i = start_count
-#     #print(i)
-#     while i < end_count:
-#         if i%2 == 0:
-#             e_x_idx = df_data[1]['Exx'][df_data[1]['File_Number'] == i].index.tolist()
-#
-#             # print(e_x_idx)
-#             # print(df_data[1]['Exx'][df_data[1]['File_Number'] == i])
-#             # print(i)
-#             e_x_list.append(df_data[1].Exx[e_x_idx])
-#             e_y_idx = df_data[1]['Eyy'][df_data[1]['File_Number'] == i].index.tolist()
-#             e_y_list.append(df_data[1].Eyy[e_y_idx])
-#         i += 1
-#     # print(e_x_list)
-#     # print(e_y_list)
-
 idx = df_separated.cycle_number[df_separated.cycle_number == cycle_number_list[0]].index.tolist()
 start_count = int(df_separated.start_count[idx])
 if start_count%2 != 0:
@@ -73,8 +49,9 @@ if start_count%2 != 0:
 end_count = int(df_separated.end_count[idx])
 total_count = end_count - start_count
 
-i = start_count
-#while i <= end_count:
+i = start_count+8
+# while i <= end_count:
+### Indent everything after this to create and save stress fields for all loads in one cycle ###
 e_x_list = []
 e_y_list = []
 x_position_list = []
@@ -95,12 +72,16 @@ e_x_list = np.array(e_x_list)
 e_y_list = np.array(e_y_list)
 x_position_list = np.array(x_position_list)
 y_position_list = np.array(y_position_list)
+
+### Trial with actual poisson ratio ###
+### Gave really weird stress fields, which changed direction almost randomly for different loads ###
 poisson = np.average(-1*(e_x_list/e_y_list))
-print(poisson)
 
 ### Define u and v according to x-stress and y-stress ###
-x_stress = E/(1-nu**2)*(e_x_list+nu*e_y_list-2*nu**2*e_x_list)
-y_stress = E/(1-nu**2)*(e_y_list-nu*e_x_list)
+### Change 'nu' to 'poisson' when wanting to use a non-constant poisson ratio ###
+x_stress = (E/(1-nu**2))*(e_x_list+nu*e_y_list-2*nu**2*e_x_list)
+y_stress = (E/(1-nu**2))*(e_y_list-nu*e_x_list)
+
 u = x_stress
 v = y_stress
 x = x_position_list
@@ -112,21 +93,52 @@ fig.set_tight_layout(True)
 cycle = cycle_number_list[0]
 load = np.round(df_data[0]['load'][df_data[0]['count'] == i].iloc[0],3)
 
-### Define grid and plot ###
-# x,y = np.meshgrid(np.linspace(-80,80,total_count),np.linspace(-110,110,total_count))
-plt.quiver(x,y,u,v)
-fig.suptitle(f"Stress field of {specimen} at cycle number {cycle} and a load of {load} [kN]")
-# plt.scatter(x_position_list,y_position_list)
-filename = f'Stress_field/{specimen}/{cycle}_{load}.jpg'
+### Plot with outliers around clamp included ###
+# plt.quiver(x,y,u,v)
+# fig.suptitle(f"Stress field of {specimen} at cycle number {cycle} and a load of {load} [kN]")
+# filename = f'Stress_field/{specimen}/{cycle}_{load}.jpg'
+# plt.savefig(filename) #Uncomment for saving file at filename location
+# #plt.show()
+# print(f'Done with {filename}')
+
+### Plot with outliers around clamp excluded ###
 CSV_dataframe = pd.DataFrame({"x":x,"y":y,"u":u,"v":v})
-CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -56) & (CSV_dataframe['y'] >= 94) & (CSV_dataframe['y'] <= 115)] = 0
-CSV_dataframe[(CSV_dataframe['x'] >= 54) & (CSV_dataframe['x'] <= 70) & (CSV_dataframe['y'] >= 60) & (CSV_dataframe['y'] <= 83)] = 0
-CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -50) & (CSV_dataframe['y'] >= -77) & (CSV_dataframe['y'] <= -50)] = 0
-CSV_dataframe[(CSV_dataframe['x'] >= 56) & (CSV_dataframe['x'] <= 80) & (CSV_dataframe['y'] >= -110) & (CSV_dataframe['y'] <= -75)] = 0
+# Order of exclusions:
+# Left-top clamp - Right-top clamp - Left-bottom clamp - Right-bottom clamp
+if specimen == 'L103':
+    CSV_dataframe[(CSV_dataframe['x'] >= -85) & (CSV_dataframe['x'] <= -56) & (CSV_dataframe['y'] >= 90) & (CSV_dataframe['y'] <= 120)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 54) & (CSV_dataframe['x'] <= 70) & (CSV_dataframe['y'] >= 60) & (CSV_dataframe['y'] <= 90)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -45) & (CSV_dataframe['y'] >= -77) & (CSV_dataframe['y'] <= -50)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 56) & (CSV_dataframe['x'] <= 80) & (CSV_dataframe['y'] >= -115) & (CSV_dataframe['y'] <= -80)] = 0
+elif specimen == 'L104':
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -55) & (CSV_dataframe['y'] >= 90) & (CSV_dataframe['y'] <= 120)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 52) & (CSV_dataframe['x'] <= 72) & (CSV_dataframe['y'] >= 65) & (CSV_dataframe['y'] <= 85)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -53) & (CSV_dataframe['y'] >= -78) & (CSV_dataframe['y'] <= -52)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 54) & (CSV_dataframe['x'] <= 74) & (CSV_dataframe['y'] >= -115) & (CSV_dataframe['y'] <= -85)] = 0
+elif specimen == 'L105':
+    CSV_dataframe[(CSV_dataframe['x'] >= -75) & (CSV_dataframe['x'] <= -50) & (CSV_dataframe['y'] >= 85) & (CSV_dataframe['y'] <= 120)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 56) & (CSV_dataframe['x'] <= 71) & (CSV_dataframe['y'] >= 60) & (CSV_dataframe['y'] <= 85)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -53) & (CSV_dataframe['y'] >= -82) & (CSV_dataframe['y'] <= -50)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 52) & (CSV_dataframe['x'] <= 75) & (CSV_dataframe['y'] >= -115) & (CSV_dataframe['y'] <= -85)] = 0
+elif specimen == 'L109':
+    CSV_dataframe[(CSV_dataframe['x'] >= -85) & (CSV_dataframe['x'] <= -55) & (CSV_dataframe['y'] >= 83) & (CSV_dataframe['y'] <= 118)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 56) & (CSV_dataframe['x'] <= 75) & (CSV_dataframe['y'] >= 56) & (CSV_dataframe['y'] <= 85)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -54) & (CSV_dataframe['y'] >= -84) & (CSV_dataframe['y'] <= -57)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 53) & (CSV_dataframe['x'] <= 75) & (CSV_dataframe['y'] >= -115) & (CSV_dataframe['y'] <= -85)] = 0
+elif specimen == 'L123':
+    CSV_dataframe[(CSV_dataframe['x'] >= -85) & (CSV_dataframe['x'] <= -54) & (CSV_dataframe['y'] >= 90) & (CSV_dataframe['y'] <= 120)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 56) & (CSV_dataframe['x'] <= 75) & (CSV_dataframe['y'] >= 62) & (CSV_dataframe['y'] <= 80)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -80) & (CSV_dataframe['x'] <= -49) & (CSV_dataframe['y'] >= -79) & (CSV_dataframe['y'] <= -50)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= 52) & (CSV_dataframe['x'] <= 75) & (CSV_dataframe['y'] >= -110) & (CSV_dataframe['y'] <= -81)] = 0
+    CSV_dataframe[(CSV_dataframe['x'] >= -81) & (CSV_dataframe['x'] <= -77) & (CSV_dataframe['y'] <= 90) & (CSV_dataframe['y'] >= -81)] = 0
 
 CSV_dataframe.to_csv(f'Stress_field/CSV_files/{specimen}_{cycle}_{load}.csv')
 
+plt.quiver(CSV_dataframe['x'],CSV_dataframe['y'],CSV_dataframe['u'],CSV_dataframe['v'])
+fig.suptitle(f"Stress field of {specimen} at cycle number {cycle} and a load of {load} [kN]")
+filename = f'Stress_field/{specimen}/{cycle}_{load}.jpg'
+#plt.savefig(filename) #Uncomment for saving file at filename location
 plt.show()
-#plt.savefig(filename)
 print(f'Done with {filename}')
-#i += 2
+
+# i += 2 #Uncomment when using while loop
